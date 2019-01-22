16d75487e2546fbe1d2fe39ce622ce8811e11724
Aaron Peterson <aaron.peterson@kloc.co.uk>
2019-01-22 14:15:51 +0000

    initial commit

---
+++ resources/js/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <nav>
+                <ul class="list-inline">
+                    <li>
+                        <router-link :to="{ name: 'home' }">Home</router-link>
+                    </li>
+                    <li class="pull-right">
+                        <router-link :to="{ name: 'login' }">Login</router-link>
+                    </li>                    <li class="pull-right">
+                    <router-link :to="{ name: 'register' }">Register</router-link>
+                </li>
+                </ul>
+            </nav>
+        </div>
+        <div class="panel-body">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
---
+++ resources/js/components/Home.vue
@@ -0,0 +1,4 @@
+<script src="../app.js"></script>
+<template>
+    <h1>Laravel 5 Vue SPA Authentication</h1>
+</template>
