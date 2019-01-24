d9fc58e444a060162d98aaf2007ae104fffdeaec
Aaron Peterson <aaron.peterson@kloc.co.uk>
2019-01-24 14:07:13 +0000

    single page component sass loading/compling working

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,3 +1,7 @@
 <template>
     <h1>Laravel 5 Vue SPA Authentication</h1>
 </template>
+
+<style lang="scss">
+    @import '~@/app.scss';
+</style>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <link href="/css/app.css" rel="stylesheet" type="text/css"/>
-
+    {{--<link href="/css/app.css" rel="stylesheet" type="text/css"/>--}}
     <link rel="icon" type="image/x-icon" href="/favicon.ico">
 
     <title>Rappel</title>
